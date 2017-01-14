2f8ea746e84a07e17d4dab6a8c9691a52f4c1722
Nadeem Shaik <venomreborn@gmail.com>
2017-01-14 19:46:31 +0530

    1

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Santhosh Kumar S Portfolio Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="stylesheet" type="text/css" href="css/core.css">
+    <link rel="stylesheet" type="text/css" href="css/elements.css">
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+    <link rel="icon" type="image/png" href="favicon.png?version=4">
+  </head>
+  <body class="preload">
+    <div class="container-fluid no-spacing">
+      <header>
+        <div class="container-fluid">
+          <div class="row-fluid">
+            <div class="col-md-12 text-center"><a href="index.php" class="brand"><img alt="SSK" src="images/logo.svg" class="default"><img alt="SSK" src="images/logo-white.svg" class="white"></a>
+              <ul class="main-nav">
+                <li><a href="index.php" title="Home" class="active">Home</a>
+                </li>
+                <li><a href="pages/about.html" title="About">About</a>
+                </li>
+                <li><a href="architecture.html" title="Architecture">Architecture</a>
+                </li>
+
+                <li><a href="photography.html" title="Photography">Photography/Graphic Design</a>
+                </li>
+
+                <li><a href="sketches.html" title="Sketches">Sketches</a>
+                </li>
+                  
+                <li><a href="pages/contact.html" title="Contact">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div class="slider fs fadein">
+        <ul class="slides">
+                <li style="background-image: url(images/portfolio/p3.png)" class="slide dark">
+                  <div class="vcenter text-center">
+                    <div class="container">
+                      <h1><img src="images/projects/1/icon.png"></h1>
+                      <a href="p1.html" class="mt-lg btn btn-primary">View Project</a>
+                    </div>
+                  </div>
+                </li>
+                <li style="background-image: url(images/portfolio/p2.png)" class="slide dark">
+                  <div class="vcenter text-center">
+                    <div class="container">
+                      <h1><img src="images/projects/2/icon.png"></h1>
+                      <a href="p2.html" class="mt-lg btn btn-primary">View Project</a>
+                    </div>
+                  </div>
+                </li>
+                <li style="background-image: url(images/portfolio/p8.png)" class="slide dark">
+                  <div class="vcenter text-center">
+                    <div class="container">
+                      <h1>Photography and Design</h1>
+                      <a href="photography.html" class="mt-lg btn btn-primary">View Projects</a>
+                    </div>
+                  </div>
+                </li>
+                <li style="background-image: url(images/portfolio/p7.png)" class="slide dark">
+                  <div class="vcenter text-center">
+                    <div class="container">
+                      <h1>Sketches</h1>
+                      <a href="sketches.html" class="mt-lg btn btn-primary">View Projects</a>
+                    </div>
+                  </div>
+                </li>
+                <li style="background-image: url(images/portfolio/p1.png)" class="slide dark">
+                  <div class="vcenter text-center">
+                    <div class="container">
+                      <h1>Cluster Housing</h1>
+                      ><a href="p3.html" class="mt-lg btn btn-primary">View Project</a>
+                    </div>
+                  </div>
+                </li>
+                <li style="background-image: url(images/portfolio/p5.png)" class="slide dark">
+                  <div class="vcenter text-center">
+                    <div class="container">
+                      <h1>NH1D Competition</h1>
+                      <a href="p3.html" class="mt-lg btn btn-primary">View Project</a>
+                    </div>
+                  </div>
+                </li>
+                </li>
+                <li style="background-image: url(images/portfolio/p4.png)" class="slide dark">
+                  <div class="vcenter text-center">
+                    <div class="container">
+                      <h1>Artist's Residence</h1>
+                      <p>Title</p><a href="p5.html" class="mt-lg btn btn-primary">View Project</a>
+                    </div>
+                  </div>
+                </li>
+                
+            
+        </ul>
+      </div>
+    </div>
+    
+    <script src="js/vendor.js"></script>
+    <script src="js/functions.js"></script>
+    <script src="modules/titan-slider/titan-slider.js"></script>
+  </body>
+</html>
